cbe11c22f83483e135c099d18fb9e900d134e28a
Vincent Ooi <vincentooi@Vincents-MacBook-Pro.local>
2018-03-28 11:34:53 +0800

    update #3 updated server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = next({
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 app.prepare().then(()=>{
-  createServer(handler).listen(3000, (err)=>{
+  createServer(handler).listen(process.env.PORT || 5000 , (err)=>{
     if (err) throw err;
     console.log('Ready on localhost:3000');
   });
-});
+);
